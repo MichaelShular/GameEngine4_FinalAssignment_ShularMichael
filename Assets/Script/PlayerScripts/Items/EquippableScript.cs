3f3e4bcaa06e6202e673624fb02f32a2d3ee97b5
Mick <mickx.shular@gmail.com>
2022-04-05 21:03:08 -0400

    added scriptable object for items, pill prefab and imported UI package

---
+++ Assets/Script/PlayerScripts/Items/EquippableScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquippableScript : ItemScript
+{
+    private bool isEquipped = false;
+    public bool equipped 
+    { 
+        get => isEquipped;
+
+        set
+        {
+            isEquipped = value;
+            OnEquipeStatusChange?.Invoke();
+        }
+    }
+    public delegate void EquipeStatusChange();
+    public event EquipeStatusChange OnEquipeStatusChange;
+
+    public override void UseItem(PlayerController playerController)
+    {
+        isEquipped = !isEquipped;
+    }
+
+}
