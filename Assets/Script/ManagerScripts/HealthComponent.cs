e9a2c692f1e761f5ef9636eae80040cb2c5e3ed4
Mick <mickx.shular@gmail.com>
2022-04-06 19:05:03 -0400

    (Adding Missed Week 11 stuff) IDamage, zombie dying, ak47 deal damage

---
+++ Assets/Script/ManagerScripts/HealthComponent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour, IDamagable
+{
+    [SerializeField]
+    private float currentHealth;
+    public float CurrentHealth => currentHealth;
+
+    [SerializeField]
+    private float maxHealth;
+    public float MaxHealth => maxHealth;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public virtual void Destory()
+    {
+        Destroy(gameObject);
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            Destory();
+        }
+    }
+
+
+
+}
